8c3369cc00787ce5e500db1b6aed1b54295ff22f
Bryan Zhang <bryanzhang938@gmail.com>
2024-05-12 00:35:32 -0700

    Implemented DataFrame creation from Spotify Web API data
    Implemented a script that fetches audio features from the Spotify API and converts them into a pandas DataFrame.

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,10 +1,10 @@
-from dotenv import load_dotenv
-import os
 import base64
-from requests import post, get
-import json
+import os
+
 import pandas as pd
-import numpy as np
+from dotenv import load_dotenv
+from requests import get, post
+from concurrent.futures import ThreadPoolExecutor
 
 load_dotenv()
 
@@ -23,8 +23,9 @@ def get_token():
     }
     data = {'grant_type': 'client_credentials'}
     result = post(url, headers=headers, data=data)
-    json_result = json.loads(result.content)
-    token = json_result['access_token']
+    result.raise_for_status()
+    token = result.json()['access_token']
+    
     return token
 
 def get_auth_header(token):
@@ -34,11 +35,71 @@ def search_for(token, search_query, *filters, limit = 10):
     headers  = get_auth_header(token)
     query_url = f'https://api.spotify.com/v1/search?q={search_query}&type={','.join(filters)}&limit={limit}'
     result = get(query_url, headers=headers)
-    json_result = json.loads(result.content)
+    result.raise_for_status()
 
-    if not json_result:
-        return None
+    return result.json()
+
+def get_playlist_tracks(token, playlist_id):
+    headers = get_auth_header(token)
+    query_url = f'https://api.spotify.com/v1/playlists/{playlist_id}/tracks'
+    result = get(query_url, headers=headers)
+    result.raise_for_status()
+
+    return result.json()
+
+def get_track_features(token, track_ids):
+    headers = get_auth_header(token)
+    query_url = f'https://api.spotify.com/v1/audio-features/'
+    result = get(query_url, headers=headers, params={'ids': ','.join(track_ids)})
+    result.raise_for_status
+
+    return result.json()
+
+def get_playlist_ids(keyword):
+    search = search_for(token, keyword, 'playlist', limit=2)
+    return [item['id'] for item in search['playlists']['items']]
+
+def get_track_ids(playlist_id):
+    tracks = get_playlist_tracks(token, playlist_id)['items']
+    return [track['track']['id'] for track in tracks]
+
+def extract_all(tracks):
+    if len(tracks) == 0:
+        return []
     
-    return json_result
+    return get_track_features(token, tracks[:100])['audio_features'] + extract_all(tracks[100:])
 
 token = get_token()
+
+keywords = [
+    "greatest hits",
+    "top 100",
+    "best songs ever",
+    "all-time favorites",
+    "most streamed songs",
+    "classic hits",
+    "legendary tracks",
+    "iconic songs",
+    "chart-toppers",
+    "record breakers",
+    "masterpieces",
+    "famous songs"
+]
+
+playlist_ids = set()
+track_ids = set()
+
+with ThreadPoolExecutor() as executor:
+    playlist_results = executor.map(get_playlist_ids, keywords)
+    for playlist_result in playlist_results:
+        playlist_ids.update(playlist_result)
+    
+    track_results = executor.map(get_track_ids, playlist_ids)
+    for track_result in track_results:
+        track_ids.update(track_result)
+
+track_ids = list(track_ids)
+track_data = extract_all(track_ids)
+track_df = pd.DataFrame(track_data)
+
+track_df.to_csv('top_tracks.csv')
